25473d0e168b54ee16e3c6ae848a22a73c041973
ALevsha <levsha26r@gmail.com>
2022-10-11 12:27:27 +0300

    add data getting

--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cryptocurrency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService = RetrofitGetInterface.create()
+        apiService.getCryptocurrency()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                result -> arrayListInit(result)
+            },{
+                error -> error.printStackTrace()
+            })
+    }
+
+    private fun arrayListInit(result: List<ResponseItem>?) {
+        TODO("Not yet implemented")
     }
 }
